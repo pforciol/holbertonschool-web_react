6f85b3bb001221e866a49c273c8722a52c3ab96e
Piroli <pforciol@student.42.fr>
2022-02-03 09:32:04 +0000

    Add task_0

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$('body').add('p').text('Holberton Dashboard');
+$('body').add('p').text('Dashboard data for the students');
+$('body').add('p').text('Copyright - Holberton School');
