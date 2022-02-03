50fb45f0d38541f212fd3998b7181448e8878722
Piroli <pforciol@student.42.fr>
2022-02-03 09:47:48 +0000

    Update way of adding to a page

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
 
-$('body').add('p').text('Holberton Dashboard');
-$('body').add('p').text('Dashboard data for the students');
-$('body').add('p').text('Copyright - Holberton School');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
