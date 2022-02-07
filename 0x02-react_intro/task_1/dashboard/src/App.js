bb632ed44442e2bd94c2203d44d9c5ffb4f95445
Piroli <pforciol@student.42.fr>
2022-02-07 17:43:08 +0000

    Add task_1

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="Holberton School Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
