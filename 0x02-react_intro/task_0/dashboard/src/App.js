3651746b3f8941310af1262be73f3aa99274cb8c
Piroli <pforciol@student.42.fr>
2022-02-07 19:27:44 +0000

    Update App.js

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -4,16 +4,16 @@ import logo from './logo.jpg';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} alt="Holberton School Logo" />
         <h1>School dashboard</h1>
-      </header>
-      <body className="App-body">
+      </div>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
-      <footer className="App-footer">
+      </div>
+      <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
-      </footer>
+      </div>
     </div>
   );
 };
