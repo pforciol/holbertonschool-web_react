da6a733ba4fa6e16913633ee5049bb7bff2ca933
Piroli <pforciol@student.42.fr>
2022-02-23 09:05:30 +0000

    Add task_4

---
+++ 0x04-React_component/task_4/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class BodySection extends React.Component {
+  render() {
+    return (
+      <div className="bodySection">
+        <h2>{this.props.title}</h2>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+BodySection.propTypes = {
+  title: PropTypes.string,
+};
+
+export default BodySection;
