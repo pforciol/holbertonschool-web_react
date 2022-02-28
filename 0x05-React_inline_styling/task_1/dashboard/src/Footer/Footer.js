f9b86a336d04cdbfbd049db2359703c1df2e35fa
Pierre Forcioli <pforciol@student.42.fr>
2022-02-28 10:20:50 +0000

    Fix footer

--- 0x05-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -4,11 +4,11 @@ import { getFooterCopy, getFullYear } from '../utils/utils';
 
 const Footer = () => {
   return (
-    <div className="App-footer">
+    <footer className="App-footer">
       <p>
         Copyright {getFullYear()} - {getFooterCopy(true)}
       </p>
-    </div>
+    </footer>
   );
 };
 
