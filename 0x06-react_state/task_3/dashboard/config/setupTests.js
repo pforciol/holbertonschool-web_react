20bad80a7408eeb179e5abbb1bf6ac891b73c3fc
Pierre Forcioli <pforciol@student.42.fr>
2022-03-28 13:38:28 +0000

    Oops, I've forgotten one line...

--- 0x06-react_state/task_3/dashboard/config/setupTests.js
+++ 0x06-react_state/task_3/dashboard/config/setupTests.js
@@ -1,5 +1,6 @@
-import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme from 'enzyme';
+
 import * as Aphrodite from 'aphrodite';
 import * as AphroditeNoImportant from 'aphrodite/no-important';
 
