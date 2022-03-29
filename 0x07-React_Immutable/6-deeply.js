968e355409095eaec2fc98191eed6dff996b2814
Pierre Forcioli <pforciol@student.42.fr>
2022-03-29 09:12:32 +0000

    Update 6

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,4 +1,8 @@
 import { Map } from 'immutable';
 
-export const mergeDeeplyElements = (page1, page2) =>
-  Map(page1).mergeDeep(Map(page2));
+export const mergeDeeplyElements = (page1, page2) => {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  map1.mergeDeep(map2);
+};
